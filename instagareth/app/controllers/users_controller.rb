192c952c99596dcfb0d42950fa6ecc8e49aa078b
Gareth Harris <gdharris1000@gmail.com>
2020-04-12 09:58:29 +0100

    update user

--- instagareth/app/controllers/users_controller.rb
+++ instagareth/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :find_user, only: [:show]
+  before_action :find_user, only: [:show, :edit, :update]
 
   def index
   end
@@ -19,7 +19,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
