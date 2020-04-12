aaa423e2c3ada66b14df02b0aecfeeac98141707
Gareth Harris <gdharris1000@gmail.com>
2020-04-12 11:06:30 +0100

    posts and image uploads

---
+++ instagareth/app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  
+  before_action :find_post, only: [:show]
+  
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:caption, :photo)
+  end
+end
--- instagareth/config/routes.rb
+++ instagareth/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   resources :users
+  resources :posts
   root 'users#index'
 end
