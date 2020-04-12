1b6de24783662afc6bd126fa9cf42126515a7d41
Gareth Harris <gdharris1000@gmail.com>
2020-04-12 09:53:19 +0100

    add user

---
+++ instagareth/app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+  before_action :find_user, only: [:show]
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+end
